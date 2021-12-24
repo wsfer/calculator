fc9a00c9273c26fd369208d268276623d9f47886
Wesley <wesley_snaine@hotmail.com>
2021-12-23 22:00:50 -0300

    create operate function

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ function multiply (n1, n2) {
 function divide (n1, n2) {
     return n1 / n2;
 }
+
+function operate (a, b, operate) {
+    switch (operate) {
+        case '+':
+            return add (a, b);
+            break;
+        case '-':
+            return subtract (a, b);
+            break;
+        case '*':
+            return multiply (a, b);
+            break;
+        case '/':
+            return divide (a, b);
+            break;
+    }
+}
